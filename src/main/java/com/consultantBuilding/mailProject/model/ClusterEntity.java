b2a832bdd6dc61a19adf84e4b2c05371d34c857a
Roberto-Lazo <42620567+RobertoLazo@users.noreply.github.com>
2022-08-31 17:44:03 -0500

    Add entities in the project

---
+++ src/main/java/com/consultantBuilding/mailProject/model/ClusterEntity.java
@@ -0,0 +1,48 @@
+package com.consultantBuilding.mailProject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cluster")
+public class ClusterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cluster_id")
+    private Long groupID;
+
+    @Column(name = "cluster_name")
+    private String groupName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "contact_in_group",
+            joinColumns = @JoinColumn(name = "cluster_id"),
+            inverseJoinColumns = @JoinColumn(name = "contact_id")
+    )
+    private List<ContactEntity> contacts;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UserEntity user;
+}
+
